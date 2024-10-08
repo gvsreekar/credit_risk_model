d0ddd29eb404ef4aaa23674162081f2178c6e680
gvsreekar <80467534+gvsreekar@users.noreply.github.com>
2024-10-08 20:43:23 +0530

    Path issue solved in FE_pipeline.py to import config.py

--- credit_risk_model/FE_pipeline.py
+++ credit_risk_model/FE_pipeline.py
@@ -3,5 +3,7 @@ import numpy as np
 import pandas as pd 
 import os
 import sys
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 import credit_risk_model.config as config 
 print(config.TARGET)
